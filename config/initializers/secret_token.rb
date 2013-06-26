aef5a6594219d143a97b0443dec6e058de6b6e81
Masaki42 <masaki.tux@gmail.com>
2013-06-26 16:26:12 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anyvi::Application.config.secret_token = '3bb57bfc87c24e1534e0a97d2f7d8fb78b8eb4e5221e51d0972696fbdce95294e53442d1c3ceefaa22a56bb333b12b932be8aa39c11ed9b15b5cb9c98127adb0'
